8bc4aff183125650e94313806113a95fe8976854
相澤 隆志 <aizawakeisuke@aisawa-no-MacBook-Air.local>
2014-05-18 19:03:10 +0900

    完成

--- WebSocketChat/ViewController.m
+++ WebSocketChat/ViewController.m
@@ -8,16 +8,39 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <SRWebSocketDelegate>
+{
+    SRWebSocket* _socket;
+    BOOL _isConnected;
+}
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
+@property (weak, nonatomic) IBOutlet UITextField *address;
+- (IBAction)connectButtonClicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextField *messageTextField;
+- (IBAction)sendButtonClicked:(id)sender;
+@property (weak, nonatomic) IBOutlet UITextView *messageTextView;
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+
 
 @end
 
 @implementation ViewController
 
+- (void)webSocketDidOpen:(SRWebSocket *)webSocket
+{
+     [webSocket send:[NSString stringWithFormat:@"%@:\n", _address.text]];
+}
+
+-(void)webSocket:(SRWebSocket *)webSocket didReceiveMessage:(id)message
+{
+    NSLog(@"didReceiveMessage: %@", [message description]);
+    _messageTextView.text = [message description]  ;
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    _isConnected = NO;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +49,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)connectButtonClicked:(id)sender {
+    if( _isConnected == NO ){
+        _isConnected = YES;
+        [_connectButton setTitle:@"Disconnect" forState:UIControlStateNormal];
+        NSURL* url = [NSURL URLWithString:[NSString stringWithFormat: @"ws://%@:8080/", _address.text]];
+        _socket = [[SRWebSocket alloc] initWithURLRequest:[NSURLRequest requestWithURL:url]];
+        _socket.delegate = self;
+        [_socket open];
+    }else{
+        _isConnected = NO;
+        [_connectButton setTitle:@"Connect" forState:UIControlStateNormal];
+        [_socket close];
+    }
+}
+- (IBAction)sendButtonClicked:(id)sender {
+    [_socket send:[NSString stringWithFormat:@"%@%@:%@\n", _messageTextView.text, _nameField.text,_messageTextField.text]];
+}
 @end
